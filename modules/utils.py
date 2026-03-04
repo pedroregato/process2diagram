0483bfe00429cc44d140610be039bc79ccf7983c
Pedro Gentil <pedro.regato@gmail.com>
2026-03-04 15:30:27 -0300

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,19 @@
-from __future__ import annotations
+# modules/utils.py
 import json
-from .schema import ProcessModel
+import dataclasses
+from modules.schema import Process
 
-def to_json(proc: ProcessModel) -> str:
-    return json.dumps(proc.model_dump(), ensure_ascii=False, indent=2)
+
+def process_to_json(process: Process) -> str:
+    """Serialize a Process to a formatted JSON string."""
+    def _serialize(obj):
+        if dataclasses.is_dataclass(obj):
+            return {k: v for k, v in dataclasses.asdict(obj).items() if v is not None}
+        return obj
+
+    data = {
+        "name": process.name,
+        "steps": [dataclasses.asdict(s) for s in process.steps],
+        "edges": [dataclasses.asdict(e) for e in process.edges],
+    }
+    return json.dumps(data, indent=2, ensure_ascii=False)
